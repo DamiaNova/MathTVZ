66b3127b793a391d89b917fe0df283f0e97bd859
Mia Blažeković <mblazekov@tvz.hr>
2019-05-26 17:12:17 +0200

    Stvaranje navigacije u headeru

--- MathTVZApp/Master/MathTVZ.Master.designer.cs
+++ MathTVZApp/Master/MathTVZ.Master.designer.cs
@@ -13,6 +13,15 @@ namespace MathTVZApp {
     public partial class MathTVZ {
         
         /// <summary>
+        /// logo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage logo;
+        
+        /// <summary>
         /// BodyForm control.
         /// </summary>
         /// <remarks>
